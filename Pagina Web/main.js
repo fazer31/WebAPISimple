77f14d0e6229afa74658ebe3afbeebfe2ec39c58
Fazer31 <fserrano3101@gmail.com>
2021-08-23 17:14:52 -0500

    se carga la lista de elementos desde el API

---
+++ Pagina Web/main.js
@@ -0,0 +1,38 @@
+
+   window.onload = function() {
+        Obtener();
+    };
+
+    function IrFormularioCrear(){
+        window.location = "Registro.html";
+    }
+
+    function Obtener(){
+
+
+        const URL_API = "http://localhost:50783//api/Usuario";
+
+       // $(".table tbody").html("");
+
+        $.get(URL_API)
+        .done(function( response ) {
+            console.log(response);
+            $.each( response, function( id, fila ) {
+                $("<tr>").append(
+                    $("<td>").text(fila.IdUsuario),
+                    $("<td>").text(fila.DocumentoIdentidad),
+                    $("<td>").text(fila.Nombres),
+                    $("<td>").text(fila.Telefono),
+                    $("<td>").text(fila.Correo),
+                    $("<td>").text(fila.Ciudad),
+                    $("<td>").append(
+                        $("<button>").data("id",fila.IdUsuario).addClass("btn btn-success btn-sm mr-1 editar").text("Editar").attr({"type":"button"}),
+                        $("<button>").data("id",fila.IdUsuario).addClass("btn btn-danger btn-sm eliminar").text("Eliminar").attr({"type":"button"})
+                    )
+                ).appendTo(".table");
+            });
+        });
+    }
+
+
+
